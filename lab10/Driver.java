546081bf92e9292873c31dbe82dc73748ec5e978
Jack Roach <87109311+jackdroach@users.noreply.github.com>
2022-04-10 17:52:24 -0400

    Lab 10

---
+++ lab10/Driver.java
@@ -0,0 +1,7 @@
+public class Driver extends Recursive {
+
+    public static void main(String[] args) {
+        System.out.println(sumDigits(1137));
+    }
+
+}
---
+++ lab10/Recursive.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+
+public class Recursive {
+
+    public static int power(int x, int n) {
+        return 0;
+    }
+
+    public static int sumDigits(int n) {
+        if (n % 10 == 0) {
+            return 0;
+        }
+
+        return n % 10 + sumDigits(n / 10);
+    }
+
+    public static void printBackwards(String word) {
+        if (word.length() == 0) {
+            return;
+        }
+
+        System.out.print(word.charAt(word.length() - 1));
+        printBackwards(word.substring(0, word.length() - 1));
+    }
+
+    public static boolean isPalindrome(String word) {
+        if (word.length() <= 1) {
+            return true;
+        }
+
+        if (word.charAt(0) != word.charAt(word.length() - 1)) {
+            return false;
+        }
+
+        return isPalindrome(word.substring(1, word.length() - 1));
+    }
+
+    public static int sumPositive(int[] array) {
+        int sum = 0;
+
+        if (array.length == 0) {
+            return sum;
+        }
+
+        if (array[0] > 0) {
+            sum += array[0];
+        }
+
+        return sum + sumPositive(Arrays.copyOfRange(array, 1, array.length));
+    }
+
+    public static int max(int[] array) {
+        int max = Integer.MIN_VALUE;
+
+        if (array.length == 0) {
+            return max;
+        }
+
+        max = array[0];
+
+        return Math.max(max, max(Arrays.copyOfRange(array, 1, array.length)));
+    }
+
+}
