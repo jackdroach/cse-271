f856ddd50c1031c3d3a87ac158d0b73213287f4d
Jack Roach <87109311+jackdroach@users.noreply.github.com>
2022-05-01 23:11:48 -0400

    Update Contact.java

--- lab13/Contact.java
+++ lab13/Contact.java
@@ -13,6 +13,9 @@ public class Contact implements Serializable {
     private String email;
     private String address;
 
+    /**
+     * Default constructor. Instantiates a new Contact.
+     */
     public Contact() {
         firstName = "";
         lastName = "";
@@ -21,6 +24,9 @@ public class Contact implements Serializable {
         address = "";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Contact)) {
@@ -33,47 +39,101 @@ public class Contact implements Serializable {
             && address.equals(c.address);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
-        return String.format("%s %s, %s, %s, %s", firstName, lastName, address, email, phoneNumber);
+        return String.format("%s %s, %s, %s, %s", firstName, lastName, address,
+            email, phoneNumber);
     }
 
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Gets phone number.
+     *
+     * @return the phone number
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Sets phone number.
+     *
+     * @param phoneNumber the phone number
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * Gets email.
+     *
+     * @return the email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets email.
+     *
+     * @param email the email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Gets address.
+     *
+     * @return the address
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Sets address.
+     *
+     * @param address the address
+     */
     public void setAddress(String address) {
         this.address = address;
     }
